a27422c3971d84e4f8bc8d84d22750e2898e04d0
TSCBennett <b.bennett@techsmith.com>
2014-08-28 15:26:32 -0400

    added footer call, fixed search bug

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@
                 <div class="content-container">
                     <p>I can't seem to find the page you're looking for. Are you sure it was written by me? If it was, and I moved it, sorry. If not, thanks for stopping by.</p>
                     <p>Would you like to look around?</p>
-                    <form class="site-search" action="/search" method="post">
+                    <form class="site-search" action="<?php echo search_url(); ?>" method="post">
                         <input type="search" class="site-search__search-term" name="term" placeholder="Search..." value>
                             <div id="text-field-container" pseudo="-webkit-texfield-decoration-container">
                                 <div id="decoration"></div>
@@ -26,23 +26,4 @@
             </article>
         </div>
     </div>
-<footer name="footer">
-      <footer id="footer-container">
-	<div id="footer-content">
-	  <p>Powered by <a href="http://www.anchorcms.com">Anchor</a>, <a href="http://jpanelmenu.com">jPanelMenu</a>, and <a href="http://www.brianbennett.org/">Barhop</a>.</p>
-	</div>
-      </div>
-</footer>
-
-    <!-- jQuery -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    
-    <!-- JpanelSlider -->
-    <script src="<?php echo theme_url('/assets/js/jpanel.min.js'); ?>"></script>
-    <script src="assets/js/jpanel.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        var JPM = $.jPanelMenu();
-        JPM.on();
-    </script>
-  </body>
-</html>
+<?php theme_include('footer'); ?>
