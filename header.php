d1b22d22319c20bfe0e0d25673169b0960bf3894
Timoh <timogos.k@live.nl>
2014-09-03 21:27:40 +0200

    Only show "More" section when logged in
    Easy huh? You smart, I wish I would have thought of that.

--- header.php
+++ header.php
@@ -44,12 +44,14 @@
 	    <?php while(categories()) : ?>
             <li class="list-item"><a href="<?php echo category_url(); ?>" title="<?php echo category_title(); ?>"><?php echo category_title(); ?> (<?php echo category_count(); ?>)</a></li>
             <?php endwhile; ?>
+        <?php if(user_authed()): ?>
             <li class="list-heading">More</li>
 	    <?php if(site_meta('twitter_account')): ?>
 	    <li class="list-item"><a href="<?php echo twitter_url(); ?>" title="Twitter">Twitter</a></li>
 	    <?php endif; ?>
 	    <li class="list-item"><a href="<?php echo rss_url(); ?>">RSS</a></li>
 	    <li class="list-item"><a href="<?php echo base_url('/admin'); ?>" title="admin">Admin</a></li>
+	    <?php endif; ?>
 	  </ul><!-- #menu -->
       </div><!-- navigation -->
     </header><!-- header -->
