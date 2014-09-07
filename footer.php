2b6303680111c123a16fd1cd983ee34e6f0bc622
TSCBennett <b.bennett@techsmith.com>
2014-09-06 22:17:51 -0400

    continued refinement on color generation. BROKEN

--- footer.php
+++ footer.php
@@ -25,14 +25,39 @@
 	    JPM.on();
       });
       
-      $(".post").each(function() {
+      /* $(".post").each(function() {
 	    var cs = window.getComputedStyle(this);
 	    if (cs.backgroundImage==="none" && cs.backgroundColor==="transparent" || cs.backgroundColor.match("0, 0, 0")) {
-		  var color = "hsl("+ ~~(Math.random()*360) +",30%,60%)";
+		  var color = "hsl("+ ~~(Math.random()*180) +",80%,50%)";
 		  $(this).css("background-color", color);   
 	    }
 	    else { return }
-      });
+      }); */
+      
+      $(".post").each(function() {
+	    var cs = window.getComputedStyle(this);
+	    if (cs.backgroundImage==="none" && cs.backgroundColor==="transparent" || cs.backgroundColor.match("0,0,0")) {
+		  postID = getID
+		  colorID = (postID % 25) + 1
+		  base3ColorID = colorID.toString(3)
+		  colorChoice = ("000").substring(base3ColorID.length) + base3ColorID
+		  red = 65 + 70*(parseInt(colorChoice.charAt(0)))
+		  green = 65 + 70*(parseInt(colorChoice.charAt(1)))
+		  blue = 65 + 70*(parseInt(colorChoice.charAt(2)))
+      
+		  $(this).css('background-color', "rgb("+red+","+green+","+blue+")")
+	    }
+	    else { return }
+	    
+      })
+      
+      function getID() {
+	    $(".post").each(function() {
+		  theID = document.getElementsByTagName('article')[0].id
+		  return theID;
+	    })  
+      }
+
     </script>
   </body>
 </html>
