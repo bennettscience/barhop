2b4adc698c4dbf2603de10c53da5bd0b3b941d37
TSCBennett <b.bennett@techsmith.com>
2014-09-19 16:42:00 -0400

    rebuild page with new styles

--- 404.php
+++ 404.php
@@ -1,26 +1,29 @@
-<?php theme_include('header'); ?>
+<<?php theme_include('header'); ?>
 <div name="404">
-        <div class="404-post-container">
-            <div class="featured" id="404">
+        <div class="post-container">
+            <?php if(page_custom_field('page-featured-image')) : ?>
+            <div class="page-featured" style="background:url('<?php echo page_custom_field('page-featured-image')?>') center center no-repeat; background-size:cover;position:relative;" >
+            <?php else: ?>
+            <div class="page-featured" style="height:250px;">
+            <?php endif; ?>
                 <div class="title">
-                    <h1>You seem to be lost</h1>
+                    <h1>Sorry, that doesn't exist</h1>
                 </div>
             </div> <!-- featured image -->
             <article class="the-post">
                 <div class="content-container">
-                    <p>I can't seem to find the page you're looking for. Are you sure it was written by me? If it was, and I moved it, sorry. If not, thanks for stopping by.</p>
-                    <p>Would you like to look around?</p>
-                    <form class="site-search" action="<?php echo search_url(); ?>" method="post">
-                        <input type="search" class="site-search__search-term" name="term" placeholder="Search..." value>
-                            <div id="text-field-container" pseudo="-webkit-texfield-decoration-container">
-                                <div id="decoration"></div>
-                                <div id="editing-view-port">
-                                        <div id="inner-editor"></div>
-                                </div>
-                                <div pseudo="-webkit-search-cancel-button" id="clear" style="opacity: 0;pointer-events:none;"></div>
-                            </div>
-                        </input>
-                    </form>
+                    <p>Try searching for the post you want using the search box below.</p>
+			<form class="site-search" action="<?php echo search_url(); ?>" method="post">
+				<input type="search" class="site-search__search-term" name="term" placeholder="Search..." value>
+					<div id="text-field-container" pseudo="-webkit-texfield-decoration-container">
+					<div id="decoration"></div>
+					<div id="editing-view-port">
+						<div id="inner-editor"></div>
+					</div>
+					<div pseudo="-webkit-search-cancel-button" id="clear" style="opacity: 0;pointer-events:none;"></div>
+					</div>
+				</input>
+			</form>
                 </div>
             </article>
         </div>
