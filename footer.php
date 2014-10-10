548ae7f63ba50932aec6fb9110c13050b1a7d1c7
bennettscience <b.bennett@techsmith.com>
2014-10-10 11:43:18 -0400

    change script calls

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
     <!-- Scripts -->
-    <!-- <script type="text/javascript" src="<?php echo theme_url('assets/js/menu.js'); ?>"></script> -->
     <script type="text/javascript" src="<?php echo theme_url('assets/js/credit.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo theme_url('assets/js/color.js'); ?>"></script>
 
