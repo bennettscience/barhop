7f5cf212d1d521a4c7ccc576cb684ceb23aaac6f
bennettscience <brian.bennett2@gmail.com>
2015-01-13 13:16:10 -0500

    update theme info

--- footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Scripts -->
-    <script type="text/javascript" src="<?php echo theme_url('assets/js/credit.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo theme_url('assets/js/color.js'); ?>"></script>
-
-  </body>
-</html>
