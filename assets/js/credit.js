10c4775df780bebc8b355efcfefb53b51904b757
bennettscience <brian.bennett2@gmail.com>
2016-03-03 21:06:26 -0500

    rework image credit JS
    Flickr added a new URL structure for absolute image references. Instead of using
    the `replace()` method, this uses a combination of the `split()` and `pop()` methods
    to find the final forward slash in the URL before splitting from the underscore for
    the photo ID.

--- assets/js/credit.js
+++ assets/js/credit.js
@@ -2,19 +2,19 @@ $('.featured').ready(function(){
 	    function extractUrl(input) {
 		  return input.replace(/"/g,"").replace(/url\(|\)$/ig, "");
 	    };
-      
-	    //Set a variable to hold the extracted URL      
+
+	    //Set a variable to hold the extracted URL
 	    bg = extractUrl($(".featured, .post").css("background-image"));
 	    extractPhotoId();
-      
+
 	    function extractPhotoId() {
-		  photoId = bg.replace(/(.+\.[a-z]{2,4})\/(\d{1,5})\/(\d{7,15})(?:(?!a-z\.-_).)*/ig, '$3')
+			photoId = bg.split('/').pop().split('_')[0];
 	    }
             console.log(photoId)
-      
+
 	    var apiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=c8c95356e465b8d7398ff2847152740e&photo_id=" + photoId + "&format=json&jsoncallback=?";
             console.log(apiUrl)
-      
+
 	    $.getJSON(apiUrl, function(data){
 	        $("#credits").html('<a href="http://www.flickr.com/photos/'+data.photo.owner.nsid+'/'+data.photo.id+'/" target="blank">'+data.photo.title._content+ ' by ' +data.photo.owner.username+"</a>");
 	    });
@@ -26,7 +26,7 @@ $("img").each(function() {
 		  // If "flickr.com" is in the URL, run the function
 		  if (!/flickr\.com$/i.test(imgSrc)) {
 			// Get the photo ID from the src
-			photoId = imgSrc.replace(/(.+\.[a-z]{2,4})\/(\d{1,5})\/(\d{7,15})(?:(?!a-z\.-_).)*/ig, '$3');
+			photoId = imgSrc.split('/').pop().split('_')[0];
 			//Call the Flicmr API to get the photo info as JSON
 			var apiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=c8c95356e465b8d7398ff2847152740e&photo_id=" + photoId + "&format=json&jsoncallback=?";
 			// Put a credit after the image using the JSON data
